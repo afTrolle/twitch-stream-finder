f641f15b1640db5040f7e982146e24f1625c38ee
Alexander af Trolle <alexander.af.trolle@gmail.com>
2018-03-03 01:54:24 +0100

    Feat: add init code for JOOQ

--- server/src/main/java/twitch/explorer/Main.java
+++ server/src/main/java/twitch/explorer/Main.java
@@ -1,8 +1,11 @@
 package twitch.explorer;
 
 
+import twitch.explorer.database.JooqHandler;
 import twitch.explorer.scraper.TwitchScrapper;
 import twitch.explorer.settings.Config;
+
+import java.sql.SQLException;
 
 public class Main {
 
@@ -10,6 +13,14 @@ public class Main {
         Config.init();
         TwitchScrapper scrapper = new TwitchScrapper();
         scrapper.start();
+
+        try {
+            JooqHandler jooqHandler = new JooqHandler();
+
+            jooqHandler.createGame("hello","http://helloworld.com");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 
